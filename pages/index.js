d1535cda4787348808079559c45afdabbc4d6abc
iAnthonyCoder <anthonymedina958@gmail.com>
2021-08-20 13:11:59 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,30 @@ export default function Home() {
             <Helmet>
                 <title>Weedzly Enterprise - Solutions for Cannabis Dispensaries</title>
                 <meta name="description" content="Solutions for Cannabis Dispensaries" />
-                <link rel="icon" type="image/png" href="/images/favicon.png"></link>
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+
+                {/* <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" /> */}
+
+
+                <meta name="twitter:image:src" content="/images/favicon.png"/>
+                <meta name="twitter:site" content="@weedzly"/>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:title" content="Weedzly"/>
+                <meta name="twitter:description" content="Solutions for Cannabis Dispensaries"/>
+                <meta property="og:image" content="/images/favicon.png"/> 
+                <meta property="og:image:alt" content="Solutions for Cannabis Dispensaries"/>
+                <meta property="og:site_name" content="Weedzly"/>
+                <meta property="og:type" content="object"/>
+                <meta property="og:title" content="Weedzly"/>
+                <meta property="og:url" content="https://www.weedzly.com"/>
+                <meta property="og:description" content="Solutions for Cannabis Dispensaries"/>
             </Helmet>
             <NavbarSaasLandingTwo selected='dispensaries' />
 
